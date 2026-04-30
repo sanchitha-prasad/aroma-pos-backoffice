4be383fbdc9c7439d92a62dcc961bfadd04e72de
Thilina <manasirmhmd@gmail.com>
2026-04-30 15:27:52 +0530

    Update URLs

--- aroma-pos-app/src/shared/services/api/config.ts
+++ aroma-pos-app/src/shared/services/api/config.ts
@@ -1,8 +1,10 @@
 export const API_CONFIG = {
     // CORE_URL: 'https://geodetic-heliographically-brandee.ngrok-free.dev/api', 
-    CORE_URL: 'http://localhost:5699', 
+    //CORE_URL: 'http://localhost:5699', 
     // AUTH_URL: 'https://geodetic-heliographically-brandee.ngrok-free.dev/auth',
-    AUTH_URL: 'http://localhost:5700',
+    //AUTH_URL: 'http://localhost:5700',
+    CORE_URL: 'https://aroma-pos-app-staging-cef4bwetdya6g3hm.southindia-01.azurewebsites.net/', 
+    AUTH_URL: 'https://aroma-pos-identity-staging-a9e3fnhcemcgdpfd.southindia-01.azurewebsites.net/',
     TIMEOUT: 150000,
     HEADERS: {
         'Content-Type': 'application/json',
